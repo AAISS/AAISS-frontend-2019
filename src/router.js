46b6ccec2ffd9f305062656ec68c4d37de246828
Hadi Tabatabaei <hadi.tabatabaei.aut@gmail.com>
2019-07-12 18:35:17 +0430

    404 router added, speaker route updated

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Speaker from './views/Speaker.vue'
+import NotFound from './views/NotFound'
 
 Vue.use(Router);
 
@@ -14,9 +15,9 @@ export default new Router({
             name: 'Home',
             component: Home
         }, {
-            path: '/speaker/id/:id',
+            path: '/speakers/:id',
             name: 'Speaker',
             component: Speaker,
-        }
+        },{ path: "*", component: NotFound }
     ]
 })
