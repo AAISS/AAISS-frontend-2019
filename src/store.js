1a27254c80442d6d4ff540490258df0d8fcc4cb6
Hadi Tabatabaei <hadi.tabatabaei.aut@gmail.com>
2019-07-12 18:36:37 +0430

    store updated

--- src/store.js
+++ src/store.js
@@ -1,16 +1,25 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  }
+    state: {
+        api: 'http://aaiss.ceit.aut.ac.ir/api',
+        speakers: [],
+    },
+    mutations: {
+        updateSpeakers(state, newSpeakers) {
+            state.speakers = newSpeakers;
+        }
+    },
+    actions: {},
+    getters: {
+        getApi: state => {
+            return state.api;
+        },
+        getSpeakers: state => {
+            return state.speakers;
+        }
+    }
 })
